3bb4a77caa70c331c2f0d22194c0032413eb4f89
Inclooder <inclooder@gmail.com>
2015-12-02 01:37:37 +0100

    code refactor

--- scwg.py
+++ scwg.py
@@ -1,10 +1,38 @@
 from xml.dom import minidom
 
+def appendTunnelPortGeneration(text):
+    text += '\n'
+    text += 'tun_port=`shuf -i 2000-65000 -n 1`\n'
+    text += 'echo "Tunnel port $tun_port"\n'
+    return text
+
+def appendBashFunctionBegin(functionName, text):
+    text += '{0}(){{\n'.format(functionName)
+    return text
+
+
+def appendBashFunctionEnd(text):
+    text += '}\n'
+    return text
+
+
+def appendSshCall(userName = 'root', ipAddress = 'localhost', port = 22, tunnelPort = None, text = ''):
+        text += 'ssh {0}@{1} '.format(userName, ipAddress)
+        if port:
+            text += '-p {0} '.format(port)
+
+        if tunnelPort:
+            text += '-L {1}:localhost:{0}\n'.format(port, tunnelPort)
+        else:
+            text += '\n'
+
+        return text
+
 
 def create_func(term):
     temp='\n'
     _name=term.getAttribute('name')
-    temp+='{0}(){{\n'.format(_name)
+    temp = appendBashFunctionBegin(_name, temp)
 
     elems=[]
     for conn in term.childNodes:
@@ -19,21 +47,11 @@ def create_func(term):
           ran_tun = False
 
         if ran_tun:
-            temp+='''
-tun_port=`shuf -i 2000-65000 -n 1`
-echo "Tunnel port $tun_port"
-'''
+            temp = appendTunnelPortGeneration(temp)
 
-        _last_ip=elems[0].getAttribute('ip')
-        temp+='ssh {0}@{1} '.format(elems[0].getAttribute('user'), elems[0].getAttribute('ip'))
         _port = elems[0].getAttribute('port') or 22
-        if _port:
-            temp+= '-p {0} '.format(_port)
-
-        if ran_tun:
-            temp+='-L $tun_port:localhost:{0}\n'.format(_port)
-        else:
-            temp+='\n'
+        _last_ip = elems[0].getAttribute('ip')
+        temp = appendSshCall(text = temp, userName = elems[0].getAttribute('user'), ipAddress = _last_ip, port = _port, tunnelPort = '$tun_port')
 
 
     for elem in elems[1:]:
@@ -46,20 +64,12 @@ echo "Tunnel port $tun_port"
       temp+='echo "Nie mozna sie polaczyc z adresem {0}"\n'.format(_last_ip)
 
       _last_ip=elem.getAttribute('ip')
-      temp+='ssh {0}@{1} '.format(elem.getAttribute('user'), elem.getAttribute('ip'))
       _port = elem.getAttribute('port') or 22
-      if _port:
-          temp+= '-p {0} '.format(_port)
-
-      if ran_tun:
-          temp+='-L $tun_port:localhost:{0}\n'.format(_port)
-      else:
-          temp+='\n'
-
+      temp = appendSshCall(text = temp, userName = elem.getAttribute('user'), ipAddress = elem.getAttribute('ip'), port = _port, tunnelPort = '$tun_port')
 
       temp+='fi\n'
 
-    temp+='}\n'
+    temp = appendBashFunctionEnd(temp)
     return temp
 
 
