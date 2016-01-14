09e26a6d87119fcf10d0d415a062ce070922dbb9
Inclooder <inclooder@gmail.com>
2016-01-14 02:03:10 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Scwg',
+      version='1.0',
+      description='Ssh connection wrapper generator',
+      author='Inclooder',
+      author_email='inclooder@gmail.com',
+      url='https://github.com/inclooder/scwg',
+      packages=['scwg']
+)
+
